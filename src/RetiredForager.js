d9ecc52f1672dba6af4ff9f3af540506acc61f4d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-18 11:30:39 -0700

    Implemented RetiredForagerBee subclass

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,22 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+//set prototype to delegate to ForagerBee prototype
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+//add back constructor property
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+//add subclass-specific methods
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
