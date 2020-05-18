2237d8a83c883bf27e270eb69c95e37a267f5c87
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-18 11:21:00 -0700

    Implemented Bee and HoneyMakerBee subclasses

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+//reset Bee subclass prototype to an empty object that delegates to Grub superclass prototype
+Bee.prototype = Object.create(Grub.prototype);
+
+//add back the constructor property that we lost due to above reset
+Bee.prototype.constructor = Bee;
+
+//add shared methods specific to Bee to the Bee prototype
+//(not applicable so far)
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+};
+
+//set prototype to delegate to Bee
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+//add back constructor property
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+//add subclass-specific methods
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
 };
 
