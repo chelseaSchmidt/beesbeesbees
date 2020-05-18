a7803645ecba7fbd59215330bd70541784712b43
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-18 11:25:13 -0700

    Implemented ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+//set prototype to delegate to Bee superclass
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+//add back constructor property
+ForagerBee.prototype.constructor = ForagerBee;
+
+//add subclass-specific methods
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
